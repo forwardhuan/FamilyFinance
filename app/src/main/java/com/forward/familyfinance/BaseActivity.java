9df8bb46abef6acbc5a76d1116186fc2b5e25c9e
forwardhuan <forward_m@126.com>
2020-02-05 13:41:34 +0800

    Create new project

---
+++ app/src/main/java/com/forward/familyfinance/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.forward.familyfinance;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
